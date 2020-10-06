de6383f6faf7cc8b7c21785a38fd08903e7a13b5
Tapan Patel <tapan@pop-os.localdomain>
2020-10-06 22:23:00 +0530

    fixed issues

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoLayout from './components/todoLayout';
 function App() {
   return (
 
-    <div className="bg-gray-800 h-screen flex flex-col items-center pt-20">
+    <div className="bg-gray-800 h-screen flex flex-col items-center pt-20 overflow-y-scroll">
       <h1 className="text-4xl text-green-400 mb-5">Todo List</h1>
       <TodoLayout />
     </div>
